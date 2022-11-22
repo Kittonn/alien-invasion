b36fac8488e0701a34a9d3155ad67f85e285e42b
Kittipod Lambangchang <tonkittipod@gmail.com>
2022-11-22 08:38:38 +0700

    chore: fix main menu

--- code/setting.py
+++ code/setting.py
@@ -7,19 +7,15 @@ screen_height = vertical_tile_number * tile_size
 main_menu_data = {
     0: {
         "name": "Play Game",
-        "node_pos": (screen_width / 2, 230)
+        "node_pos": (screen_width / 2, 260)
     },
     1: {
         "name": "Leaderboard",
-        "node_pos": (screen_width / 2, 350)
-    },
-    2: {
-        "name": "How to play",
-        "node_pos": (screen_width / 2, 470)
+        "node_pos": (screen_width / 2, 410)
     },
     3: {
         "name": "Quit",
-        "node_pos": (screen_width / 2, 590)
+        "node_pos": (screen_width / 2, 560)
     },
 
 }
